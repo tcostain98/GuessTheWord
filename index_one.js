a5d1de7f2bd944828999f666804748ed40aa9613
tcostain98 <tomcostain7@me.com>
2021-10-13 13:56:46 +0000

    addint worfd to the game

--- index_one.js
+++ index_one.js
@@ -1,3 +1,3 @@
-words_easy = []
-words_medium = []
-words_hard = []
+words_easy = ["jazz","buzz", "jinx", "fox", "six", "fizz", "blue", "grey", "dog", "phone", "hello",]
+words_medium = ["livid", "buzzing", "appreciate", "friend", "uncle", "football", "royal", "happy", "monkey", "reason"]
+words_hard = ["millennium", "ridiculous", "liaison", "requirements", "logically", "kitchen", "dictionaries", "pneumonia", "embarrassed", "accommodate", ]
