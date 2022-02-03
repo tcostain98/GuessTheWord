2e2835ba9b9b40aee00158a663f0e9b6cec3b1f9
tcostain98 <tomcostain7@me.com>
2022-02-03 12:08:28 +0000

    attempting to fix code

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ def play_word(current_word, masked_word, lives):
         if (validate_letter_in_word(current_letter, current_word)):
             masked_word = update_masked_word(
                 current_word, masked_word, current_letter)
-        user_won_game = check_if_user_won(current_word, masked_word)
+            user_won_game = check_if_user_won(current_word, masked_word)
         else:
             lives -= 1
         game_result(user_won_game, current_word)
